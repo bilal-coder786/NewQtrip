cffa19342eafa4295e2ed7fb88b524c86f985377
Mohammad Bilal <mohammad.bilaal786@gmail.com>
2025-07-09 21:50:05 +0530

    Updated ExtentManager for dynamic Extent report path

--- app/src/test/java/qtriptest/utils/ExtentManager.java
+++ app/src/test/java/qtriptest/utils/ExtentManager.java
@@ -1,3 +1,22 @@
+// package qtriptest.utils;
+
+// import com.aventstack.extentreports.ExtentReports;
+// import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+// public class ExtentManager {
+//     private static ExtentReports extent;
+
+//     public static ExtentReports getInstance() {
+//         if (extent == null) {
+//             ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
+//             extent = new ExtentReports();
+//             extent.attachReporter(spark);
+//             extent.setSystemInfo("Environment", "QA");
+//             extent.setSystemInfo("User", "Mohammad Bilal");
+//         }
+//         return extent;
+//     }
+// }
 package qtriptest.utils;
 
 import com.aventstack.extentreports.ExtentReports;
@@ -8,9 +27,18 @@ public class ExtentManager {
 
     public static ExtentReports getInstance() {
         if (extent == null) {
-            ExtentSparkReporter spark = new ExtentSparkReporter("test-output/ExtentReport.html");
+            // Get current working directory
+            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
+
+            // Print path to console for debugging
+            System.out.println("📄 Extent Report will be generated at: " + reportPath);
+
+            // Create reporter and attach to ExtentReports
+            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
             extent = new ExtentReports();
             extent.attachReporter(spark);
+
+            // Add system info
             extent.setSystemInfo("Environment", "QA");
             extent.setSystemInfo("User", "Mohammad Bilal");
         }
